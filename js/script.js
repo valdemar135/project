5d1b6879583822346ac5e53a898b0b942c182cf4
Sergey <example@gmail.com>
2021-12-12 16:37:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+//alert('Hello');
+//const result = confirm('are you here? ');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?','18');
+//console.log( typeof(answer) );
+const answers = [];
+answers[0] = prompt('Как ваше имя?','');
+answers[1] = prompt('Как ваша фамилия?','');
+answers[2] = prompt('сколько вам лет?','');
+
+document.write(answers);
